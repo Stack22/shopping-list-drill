90ce9726d02b187593d6cd92db1cc76e8564c68a
Stack22 <stacyking1421@gmail.com>
2017-01-26 15:57:35 -0500

    add item + html working, page auto-refreshes

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Shopping list item
+var shoppingList = {
+  items: []
+};
+
+// Add items to list
+var addToList = function(list, item) {
+  console.log("starting addToList");
+  list.items.push(item);
+};
+
+// Remove items from list
+var remFromList = function(list, item) {
+  var arrayLoc = list.indexOf(item);
+  if (list.items === item) {
+    list.splice(arrayLoc, 1);
+  };
+};
+
+// "Check" items on list (toggleClass ?)
+
+
+// "Un-check" items on list
+
+
+// Render functions
+var renderList = function(list, element) {
+  console.log("starting renderList")
+  var itemsHTML = list.items.map(function(item) {
+    return '<li><span class="shopping-item">' + $(".js-input").val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
+      });
+      console.log("itemsHTML: " + itemsHTML);
+      element.html(itemsHTML);
+};
+
+// Do all the things
+$(":submit").click(function(event) {
+  addToList(shoppingList, $(".js-input").val());
+  renderList(shoppingList, $("#js-ul-parent"));
+});
