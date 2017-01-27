7309908b288e5b9bd98c32faa2d6e834e6a7029d
Stack22 <stacyking1421@gmail.com>
2017-01-27 14:14:17 -0500

    bug- deleting only last element

--- js/app.js
+++ js/app.js
@@ -9,22 +9,25 @@ var addToList = function(list, item) {
   if (list.items.includes(item)) {
     // toggle checked
     console.log("item already in list");
+    $(".js-input").val("");
   } else {
     list.items.push(item);
     // list.prehtml.push('<li><span class="shopping-item js-item">');
     // list.posthtml.push('</span><div class="shopping-item-controls"><button  class="shopping-item-toggle"><span class="button-label js-check">check</span></button><button class="shopping-item-delete js delete"><span class="button-label">delete</span></button></div></li>');
     $(".js-input").val("");
   };
-
 };
 
 // Remove items from list
 var remFromList = function(list, item) {
   console.log("start remFromList");
-  var arrayLoc = list.indexOf(item);
-  if (list.items === item) {
-    list.splice(arrayLoc, 1);
-  };
+  console.log(list + ' ... ' + item);
+  var arrayLoc = list.items.indexOf(item);
+  console.log(arrayLoc);
+  if (list.items[arrayLoc] === item) {
+    list.items.splice(arrayLoc, 1);
+  }
+  console.log(shoppingList);
 };
 
 // "Check" items on list (toggleClass ?)
@@ -38,7 +41,7 @@ var renderList = function(list, element) {
   console.log("starting renderList")
   $(".js-ul-parent").html("");
   var itemsHTML = list.items.map(function(item) {
-    return '<li><span class="shopping-item js-item">' + item + '</span><div class="shopping-item-controls"><button  class="shopping-item-toggle"><span class="button-label js-check">check</span></button><button class="shopping-item-delete js delete"><span class="button-label">delete</span></button></div></li>'
+    return '<li><span class="shopping-item js-item">' + item + '</span><div class="shopping-item-controls"><button  class="shopping-item-toggle"><span class="button-label js-check">check</span></button><button class="shopping-item-delete"><span class="button-label js-delete">delete</span></button></div></li>'
       });
       // console.log("itemsHTML: " + itemsHTML);
       element.html(itemsHTML);
@@ -52,12 +55,12 @@ var doTheThing = function() {
     renderList(shoppingList, $("#js-ul-parent"));
     console.log(shoppingList);
   });
-  $(".js-delete").click(function(event) {
-    event.preventDefault();
-    // remFromList(shoppingList, $(this).closest("js-item").val());
-    console.log("Remove: " + $(event).closest("js-item").val());
+  $("#js-ul-parent").click(function(event) {
+    event.stopImmediatePropagation();
+    console.log("Remove: " + $("js-item").text());
+    remFromList(shoppingList, $(this).closest("span").val());
     renderList(shoppingList, $("#js-ul-parent"));
-    console.log(shoppingList);
+    console.log(shoppingList.items);
   });
 };
 
