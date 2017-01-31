8e20fc878330df71d4782409525053d40bf96450
Stack22 <stacyking1421@gmail.com>
2017-01-31 18:01:31 -0500

    debugged check not working... found (var name change)... now working

--- js/app.js
+++ js/app.js
@@ -7,10 +7,15 @@ var state = {
 
 var listItemTemplate = (
   '<li><span class="shopping-item js-item">'+
-  '</span><div class="shopping-item-controls"><button  class="shopping-item-toggle"><label class="button-label" id="#js-check-item">check</label></button><button class="shopping-item-delete"><label class="button-label" id="js-delete-item" >delete</label></button></div></li>'
+  '</span><div class="shopping-item-controls"><button  class="shopping-item-toggle js-shopping-item-toggle"><span class="button-label" id="js-check-item">check</span></button><button class="shopping-item-delete"><span class="button-label" id="js-delete-item" >delete</span></button></div></li>'
 )
 // Add items to list
-var addToList = function(state, item) {
+
+function getItem(state, itemIndex) {
+  return state.items[itemIndex];
+}
+
+function addToList(state, item) {
   if (state.items.hasOwnProperty(item)) { // not working
     // need to make sure un-checked (since already on list)
     alert("That item already exists on list!");
@@ -34,17 +39,19 @@ function deleteItem(state, itemIndex) {
 //   }
 // };
 
-
+function updateItem(state, itemID, newItemState) {
+  state.items[itemID] = newItemState;
+}
 
 // Render functions (DOM manipulation)
-function renderItem(item, itemId, itemTemplate, itemDataAttr) {
+function renderItem(item, itemPos, itemTemplate, itemDataAttr) {
   var element = $(itemTemplate);
   element.find('.js-item').text(item.itemName);
-  if (item.checkedOff) {
+  if (item.itemChecked) {
     element.find('.js-item').addClass('shopping-item__checked');
   }
   element.find('.js-shopping-item-toggle')
-  element.attr(itemDataAttr, itemId);
+  element.attr(itemDataAttr, itemPos);
   return element;
 }
 
@@ -76,17 +83,27 @@ function delAnItem(state, formElement, delButtonID, itemDataAttr, listElement) {
   });
 }
 
-// function delAnItem(state, )
-// Do all the things
+function checkAnItem(state, listElement, checkButtonID, itemDataAttr) {
+  listElement.on('click', checkButtonID, function(event) {
+    var itemPos = $(event.currentTarget.closest('li')).attr(itemDataAttr);
+    var oldItem = getItem(state, itemPos);
+    updateItem(state, itemPos, {
+      itemName: oldItem.itemName,
+      itemChecked: !oldItem.itemChecked
+    });
+    renderList(state, listElement, itemDataAttr);
+  });
+}
+
 $(function() {
   var formElement = $("#js-shopping-list-form");
   var listElement = $("#js-ul-parent");
   var itemID = "#js-new-item";
   var delButtonID = "#js-delete-item";
-  var checkButtonID = "#js-check-item";
+  var checkButtonID = ".js-shopping-item-toggle";
   var itemDataAttr = "data-list-item-id";
 
   addAnItem(state, listElement, formElement, itemID, itemDataAttr);
   delAnItem(state, formElement, delButtonID, itemDataAttr, listElement);
-
+  checkAnItem(state, listElement, checkButtonID, itemDataAttr);
 })
